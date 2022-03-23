bd8e97590ad1ed2243deccd389335c947aa8da78
KieceDonc <valentin.verstracte@gmail.com>
2022-03-23 20:28:52 +0100

    Small edit

--- projet.cpp
+++ projet.cpp
@@ -16,41 +16,45 @@ typedef std::vector<int> Matrice1D;
 typedef std::vector<Matrice1D> Matrice2D;
 typedef std::map<int, int> Translation;
 
+Matrice2D adjacence;
 Translation translate;
 int uniqueVertexCount = 0;
 
-void showGraphe(Matrice2D adjacence){    
+void showGraphe(){    
     std::ofstream output;
     output.open("Graphe.ps",std::ios::out);
     output << "%!PS-Adobe-3.0" << std::endl;
     output << std::endl;  
 
-    for(int x = 0; x < uniqueVertexCount; x++){
-        int max = 1;
+    int max = 1;
+    for(int x = 0; x < uniqueVertexCount; x++){       
         for(int y = 0; y < max; y++){
+            std::cout << adjacence[x][y] << " ";
+
             if(adjacence[x][y] == 1){
+                std::cout << "yes yes yes " << std::endl;
                 output << x*10 << " " << y*10 << " 1 0 360 arc" <<std::endl;
                 output << "0 setgray" << std::endl;
                 output << "fill" << std::endl;
                 output << "stroke" << std::endl;
+                output << x*10 << " " << y*10 << " moveto" << std::endl;
+                output << x*10 << " " << y*10 << " lineto" << std::endl;
                 output << std::endl;
             }
         }
+        std::cout << "max = "<< max << std::endl;
         max+=1;
     } 
     output << std::endl;  
 }
 
 
-int translateVertex(Translation translate, int vertex){
+int translateVertex(int vertex){
     if (translate.find(vertex) != translate.end()){
-        std::cout << "founded" << std::endl;
         return translate[vertex];
     }else{
         translate.insert(std::pair<int, int>(vertex, uniqueVertexCount));
-        //translate[input]=uniqueVertexCount;
         uniqueVertexCount+=1;
-        std::cout << "notfounded" << std::endl;
         return translate[vertex];
     }
 }
@@ -61,25 +65,22 @@ int numberOfUniqueVertex(char fileUri[]){
     int vertexA, vertexB;
 
     while (infile >> vertexA >> vertexB){
-        translateVertex(translate,vertexA);
-        translateVertex(translate,vertexB);
-        std::cout << vertexA << " " << vertexB << " " << uniqueVertexCount << " " << std::endl;
-
+        translateVertex(vertexA);
+        translateVertex(vertexB);
     }
 
     return uniqueVertexCount;
 }
 
-void fillMatrice(char fileUri[], Matrice2D adjacence){
+void fillMatrice(char fileUri[]){
     std::ifstream infile(fileUri);
 
     int vertexA, vertexB;
 
     while (infile >> vertexA >> vertexB){
 
-        vertexA = translateVertex(translate,vertexA);
-        vertexB = translateVertex(translate,vertexB);
-        std::cout << vertexA << " " << vertexB << " " << uniqueVertexCount << " " << std::endl;
+        vertexA = translateVertex(vertexA);
+        vertexB = translateVertex(vertexB);
         adjacence[vertexA][vertexB] = 1;
         adjacence[vertexB][vertexA] = 1;
     }
@@ -92,9 +93,9 @@ int main(){
     uniqueVertexCount = 0;
     numberOfUniqueVertex(fileUri);
 
-    Matrice2D adjacence(uniqueVertexCount,Matrice1D(uniqueVertexCount)); 
-    fillMatrice(fileUri,adjacence);
+    adjacence = Matrice2D(uniqueVertexCount,Matrice1D(uniqueVertexCount)); 
+    fillMatrice(fileUri);
 
-    showGraphe(adjacence);
+    showGraphe();
     return 0;
 }
